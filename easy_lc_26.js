fe56f966bfd77f803e444b32492a46f88fa7846f
anujgawde <gawdeanuj@gmail.com>
2025-12-23 02:14:19 -0500

    initial commit: solved remove duplicates from sorted array

---
+++ easy_lc_26.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function (nums) {
+  for (var i = nums.length - 2; i >= 0; i--) {
+    if (nums[i] === nums[i + 1]) {
+      nums.splice(i, 1);
+    }
+  }
+
+  return nums.length;
+};
