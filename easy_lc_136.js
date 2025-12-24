08d6008f943720f962992b85ecfbd8be1cf1bc5f
anujgawde <gawdeanuj@gmail.com>
2025-12-24 15:26:34 -0500

    Leetcode 136: Single Number

---
+++ easy_lc_136.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNumber = function (nums) {
+  var res = 0;
+
+  for (var i = 0; i < nums.length; i++) {
+    res = nums[i] ^ res;
+  }
+
+  return res;
+};
