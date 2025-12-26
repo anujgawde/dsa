26e851d1b36c66e819e576ff15f73f241f1a7fe8
anujgawde <gawdeanuj@gmail.com>
2025-12-26 03:53:17 -0500

    Leetcode 283: Move Zeroes

---
+++ easy_lc_283.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function (nums) {
+  let insertPos = 0;
+
+  for (const num of nums) {
+    if (num !== 0) {
+      nums[insertPos] = num;
+      insertPos++;
+    }
+  }
+
+  while (insertPos < nums.length) {
+    nums[insertPos] = 0;
+    insertPos++;
+  }
+};
