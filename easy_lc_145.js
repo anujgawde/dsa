41d7cc2e564ec9fb21d6681270b50c296cde0eb1
anujgawde <gawdeanuj@gmail.com>
2026-01-06 19:35:35 -0500

    Leetcode 145: Binary Tree Postorder Traversal

---
+++ easy_lc_145.js
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var postorderTraversal = function (root) {
+  var res = [];
+  if (!root) return res;
+
+  var stack = [root];
+
+  while (stack.length > 0) {
+    var poppedElement = stack.pop();
+    res.push(poppedElement.val);
+
+    if (poppedElement.left) {
+      stack.push(poppedElement.left);
+    }
+    if (poppedElement.right) {
+      stack.push(poppedElement.right);
+    }
+  }
+
+  return res.reverse();
+};
